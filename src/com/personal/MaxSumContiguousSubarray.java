4d232da1fdb2df619e350bd3e0aa0c0cae1e7d0d
prithwin <getprith@gmail.com>
2017-04-03 11:07:35 +0530

    Max sum subarray questions

--- src/com/personal/MaxSumContiguousSubarray.java
+++ src/com/personal/MaxSumContiguousSubarray.java
@@ -4,35 +4,74 @@ package com.personal;
  * Created by pr250155 on 3/22/17.
  */
 public class MaxSumContiguousSubarray {
-    public static void main(String[] args) {
-        int[] target = {2,4,5,22,111};
-        int MAX_SIZE=1;
+
+    public void maxSumContiguousSubarray(int[] target , int size){
         int initialSum = 0;
         int currentMax = 0;
+        int lastFrameSum = 0;
         int maxFrameX = 0;
         int maxFrameY = 0;
-        for(int i = 0, j = 0 ; j < target.length -1 ;){
+        for(int i = 0, j = 0 ; j < target.length ;){
             int newFrameSum = 0;
-            if(j<MAX_SIZE){
+            if(j<=size-1){
                 //j is not completely initialized
                 initialSum += target[j];
                 j++;
                 newFrameSum = initialSum;
+                currentMax = newFrameSum;
+                lastFrameSum = newFrameSum;
+                maxFrameX = i;
+                maxFrameY = j;
             } else {
                 //now we have the frame ready to move So well move it computing the sum
                 i++;
                 j++;
-                newFrameSum = currentMax - target[i-1] + target[j];
+                newFrameSum = lastFrameSum - target[i-1] + target[j-1];
+                lastFrameSum = newFrameSum;
+                if(newFrameSum > currentMax){
+                    currentMax = newFrameSum;
+                    maxFrameX = i;
+                    maxFrameY = j;
+                }
             }
-            if(newFrameSum > currentMax){
-                currentMax = newFrameSum;
-                maxFrameX = i;
-                maxFrameY = j;
-            }
+
         }
-        System.out.println("the max value is: "+currentMax+" with the combination: ");
-        for(int i = maxFrameX ; i<=maxFrameY ; i++){
-            System.out.println(target[i]);
+        System.out.print("the max value is: "+currentMax+" with the combination: ");
+        for(int i = maxFrameX ; i<maxFrameY ; i++){
+            System.out.print(target[i]+((i==maxFrameY-1)?"":" , "));
         }
     }
+    public static void main(String[] args) {
+        int[] target = {1,2,-3,4,5};
+        int MAX_SIZE=5;
+        new MaxSumContiguousSubarray().maxSumContiguousSubarray(target,MAX_SIZE);
+        new MaxSumContiguousSubarray().maxSumContiguousSubarray(target);
+
+    }
+
+    private void maxSumContiguousSubarray(int[] target) {
+        int maxAtPoint = 0 ;
+        int maxStartPoint=0;
+        int maxEndPoint=0;
+        int maxSoFar = Integer.MIN_VALUE;
+        for(int i = 0 ; i < target.length ; i++){
+            maxAtPoint += target[i];
+            if(maxAtPoint > maxSoFar){
+                maxSoFar = maxAtPoint;
+                maxEndPoint = i;
+            }
+
+            if(maxSoFar < 0 ){
+                maxAtPoint = 0;
+                maxStartPoint=i+1;
+            }
+
+        }
+        System.out.printf("\nthe max Sum contiguous Sub-array is %d with the combination: ",maxSoFar);
+        for(int i = maxStartPoint ; i<=maxEndPoint ; i++){
+            System.out.print(target[i]+((i==maxEndPoint)?"":" , "));
+        }
+    }
+
+
 }
