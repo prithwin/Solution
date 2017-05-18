58f0b6cd81bac5247efaab4a9bf27b5eab32c80e
prithwin <getprith@gmail.com>
2017-05-18 21:04:49 +0530

    more prep

--- src/com/random/test/JavaStreamTest.java
+++ src/com/random/test/JavaStreamTest.java
@@ -1,25 +1,31 @@
 package com.random.test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
+import sun.net.TelnetInputStream;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by pr250155 on 5/17/17.
  */
 public class JavaStreamTest {
-    public static void main(String[] args) {
-        byte[] test = new byte[3];
-//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-//        bos.write(65);
-
-        ByteArrayInputStream bis = new ByteArrayInputStream(test);
-        test[0] = 65;
-        System.out.println(bis.read());
-        test[1] = 66;
-        System.out.println(bis.read());
-        test[2] = 67;
-        System.out.println(bis.read());
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        while(true) {
+            Socket socket = serverSocket.accept();
+            BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+            BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+            InputStreamReader reader = new InputStreamReader(bis);
+            char[] buffer = new char[100000];
+            reader.read(buffer);
+            String url = new String(buffer);
+            System.out.println(url);
+            byte[] response = "HTTP/1.1 200 OK\nContent-Length: 51\nContent-Type: text/html\nConnection: Closed\n<html><head></head><body>chowbakasoma</body></html>".getBytes();
+            bos.write(response);
+            bos.flush();
+            socket.close();
+        }
 
     }
 }
