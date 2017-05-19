d910a98f9eca6470848e2ddefb9f5a106c08a340
prithwin <getprith@gmail.com>
2017-05-20 00:58:24 +0530

    more prep

---
+++ src/com/random/test/ClonableTest.java
@@ -0,0 +1,18 @@
+package com.random.test;
+
+/**
+ * Created by pr250155 on 5/20/17.
+ */
+public class ClonableTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        AB ab = new AB();
+        ab.clone();
+    }
+}
+
+class AB implements Cloneable {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
